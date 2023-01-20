74acdc6d977369a638efd472105bd9f5ef84bf95
deniseS85 <denisabel78@hotmail.de>
2023-01-20 12:24:05 +0100

    position game-over and btn

--- script.js
+++ script.js
@@ -74,16 +74,16 @@ function checkWin() {
         winner = field[2];
         document.getElementById('line8').style.transform = 'rotate(-45deg) scaleX(1)';
     }
-    /* if (winner) {
+    if (winner) {
         gameOver = true;
         // Game-Over Bild wird nach 1 Sekunde angezeigt
         setTimeout(function() {
             document.getElementById('game-over').classList.remove('d-none');
             document.getElementById('restart-btn').classList.remove('d-none');
         }, 1000);
-    } */
+    }
 }
-/* 
+
 function restart() {
     gameOver = false;
     field = [];
@@ -99,4 +99,4 @@ function restart() {
         document.getElementById('circle' + i).classList.add('d-none');
         document.getElementById('cross' + i).classList.add('d-none'); 
     }
-} */
+}
