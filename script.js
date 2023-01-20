3ee44d8a0e3637a652987f050348cf437ea4c23e
deniseS85 <denisabel78@hotmail.de>
2023-01-20 11:58:17 +0100

    responsiv lines (absolute)

--- script.js
+++ script.js
@@ -70,20 +70,20 @@ function checkWin() {
         document.getElementById('line7').style.transform = 'rotate(45deg) scaleX(1)';
     }
     //diagonal rechts/links
-    if (field[2] == field[4] && field[4] && field[6] && field[2]) {
+    if (field[2] == field[4] && field[4] == field[6] && field[2]) {
         winner = field[2];
         document.getElementById('line8').style.transform = 'rotate(-45deg) scaleX(1)';
     }
-    if (winner) {
+    /* if (winner) {
         gameOver = true;
         // Game-Over Bild wird nach 1 Sekunde angezeigt
         setTimeout(function() {
             document.getElementById('game-over').classList.remove('d-none');
             document.getElementById('restart-btn').classList.remove('d-none');
         }, 1000);
-    }
+    } */
 }
-
+/* 
 function restart() {
     gameOver = false;
     field = [];
@@ -99,4 +99,4 @@ function restart() {
         document.getElementById('circle' + i).classList.add('d-none');
         document.getElementById('cross' + i).classList.add('d-none'); 
     }
-}
+} */
